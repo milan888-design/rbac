b384f5e2b4e309ccb4602943fef1e65a5c0bd990
Milan Patel <milan888@yahoo.com>
2025-06-24 09:07:06 -0400

    Update rbac data operation allow deny check.sql

--- src/rbac data operation allow deny check.sql
+++ src/rbac data operation allow deny check.sql
@@ -1,5 +1,32 @@
---need to find the object_key for the object you are dealing with
---if the object is listed in the object table then it is under rbac
+--for the current session, it is necessary to know if role type is AllowAllDenySpecific or DenyAllAllowSpecific
+--roles are assigned to a user. User may pick one of those roles. 
+--The followin SQL to find role_type for a user for the selected role
+SELECT st_role_user.user_key, st_role_user.role_key,st_role.role_type
+ FROM st_role_user,st_role
+  WHERE
+   st_role.role_key=st_role_user.role_key
+    and st_role_user.user_key='demomanager4'
+	and st_role_user.role_key='rolekey1'
+	
+--to list role type for role_key
+select role_key,role_type from st_role;
+--"rolekey1"	"AllowAllDenySpecific"
+--"rolekey2"	"DenyAllAllowSpecific"
+
+--the main purpose of role to put in two types: AllowAllDenySpecific and DenyAllAllowSpecific
+--For super user or admin roles, role type should be AllowAllDenySpecific,
+--thus, you only need to maintain what is not allowed. 
+--This should be very small list since admin is allowed to have most objects with most data operations.
+
+--For all non super users, role type should be DenyAllAllowSpecific.
+--thus, you only need to maintain what is allowed, everything else is not allowed.
+--This should be the list that is allowed for the user
+
+
+--need to find the object_key in the rbac object table for the object you are dealing with
+--if the object is listed in the object table then it is under rbac.
+--if the object is not listed, then, it should be listed
+--or decide to let the user access the object and perform the data operation 
 SELECT object_key
       ,object_description
       ,object_type
@@ -14,12 +41,9 @@ SELECT object_key
     and object_database='test_rbac'
 	 and object_table='test_table'
 
---for the current session you need to know if role type is AllowAllDenySpecific or DenyAllAllowSpecific 
-select role_key,role_type from st_role;
---"rolekey1"	"AllowAllDenySpecific"
---"rolekey2"	"DenyAllAllowSpecific"
 
---if the role is allowalldenysome, then, check if you object you have is in deny list for the operation
+--if the role is AllowAllDenySpecific, then, check if you object you have is in deny list for the operation
+--role key, object key and operation name are needed to find the allow_deny flag. N means not allowed
 --if allow_deny ='N' then not allowed
 SELECT role_object_key
       ,role_key
@@ -36,8 +60,12 @@ SELECT role_object_key
 --allow_deny = N
 
 
---if the role is denyallallowspecific, then
---the answer has to be Y, then allowed else not allowed.	 
+ 
+--if the role is DenyAllAllowSpecific, then,
+--check if you object you have is in allow list for the operation
+--role key, object key and operation name are needed to find the allow_deny flag. Y means not allowed
+--if allow_deny ='Y' then allowed
+--the following query is same as one for 
 SELECT role_object_key
       ,role_key
       ,object_type
